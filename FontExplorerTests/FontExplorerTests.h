980b1daa45a88dcc78874db6073a8d72fb4277b3
Jon Nolen <jon.nolen@gmail.com>
2013-01-24 11:37:57 -0500

    Initial Commit

---
+++ FontExplorerTests/FontExplorerTests.h
@@ -0,0 +1,13 @@
+//
+//  FontExplorerTests.h
+//  FontExplorerTests
+//
+//  Created by Jonathan Nolen on 1/24/13.
+//  Copyright (c) 2013 Developertown. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FontExplorerTests : SenTestCase
+
+@end
---
+++ FontExplorerTests/FontExplorerTests.m
@@ -0,0 +1,32 @@
+//
+//  FontExplorerTests.m
+//  FontExplorerTests
+//
+//  Created by Jonathan Nolen on 1/24/13.
+//  Copyright (c) 2013 Developertown. All rights reserved.
+//
+
+#import "FontExplorerTests.h"
+
+@implementation FontExplorerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FontExplorerTests");
+}
+
+@end
